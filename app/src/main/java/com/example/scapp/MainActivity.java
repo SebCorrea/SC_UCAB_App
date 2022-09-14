edee4833f0ffb86c1e127d952c3571d301d36cfb
SebCorrea <sebcorrea992@gmail.com>
2022-09-14 00:24:34 -0400

    Initial commit

---
+++ app/src/main/java/com/example/scapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.scapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.CalendarView;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CalendarView CalendarView = findViewById(R.id.calendarView);
+
+
+    }
+}
